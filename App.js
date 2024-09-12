610a9a21894875ec539ad70d5e8b2e3a7be13d5c
himanshukushwaha71855 <159442084+himanshukushwaha71855@users.noreply.github.com>
2024-09-12 19:32:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+import Background from './Components/Background';
+import Forground from './Components/Forground';
+
+
+function App() {
+  
+  return (
+    <>  
+ 
+    <div className="w-full h-screen bg-zinc-800 relative">
+      
+<Background/>
+<Forground/>
+
+   </div>
+    </>
+
+    
+  );
+};
+
+
+export default App;
+
+
+
+
+
+
+
+
+//    {<TodoList/>}
+       /* <Card/> */
+     /* <Card2/>
+      <Fruits />
+      <br /><br />
+      <Contexts/>
+      <br /><br />
+      <Memo/> */
